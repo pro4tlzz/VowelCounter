2a65a5f03a3ac85c88c00d3b0f761efc3529383b
Pro4TLZZ <superfrillz12@hotmail.co.uk>
2018-08-15 11:17:37 +0100

    Initial Commit

--- VowelCounter/Program.cs
+++ VowelCounter/Program.cs
@@ -10,6 +10,19 @@ namespace VowelCounter
     {
         static void Main(string[] args)
         {
+            // Assigns Vowels in English (UK)
+            var vowelList = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            // Asks user for Input
+            Console.WriteLine("Enter something");
+            // Assigns user input to string and converts string to lowerCase
+            string userInput = Console.ReadLine().ToLower();
+            // Counts amount of vowels from userInput
+            int total = userInput.Count(c => vowelList.Contains(c));
+            // Informs user of how many vowels in input
+            Console.WriteLine("The number of vowels in your sentence is : {0}", total);
+            // Displays Text
+            Console.ReadLine();
+
         }
     }
 }
